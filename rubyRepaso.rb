32739c4532dd9b61da082305aa6e6c809367e934
realisticattorney <g.axl.aquila@gmail.com>
2020-12-20 22:15:26 -0300

    add  arrays methods

--- rubyRepaso.rb
+++ rubyRepaso.rb
@@ -510,3 +510,28 @@ a.concat(b)   #=> [1, 2, 3, 3, 4, 5]
 [1, 1, 1, 2, 2, 3, 4] - [1, 4]  #=> [2, 2, 3]
 
 
+
+
+
+
+
+
+array methods (over 150, here just a few)
+
+[].empty?               #=> true
+[[]].empty?             #=> false
+[1, 2].empty?           #=> false
+
+[1, 2, 3].length        #=> 3
+
+[1, 2, 3].reverse       #=> [3, 2, 1]
+
+[1, 2, 3].include?(3)   #=> true
+[1, 2, 3].include?("3") #=> false
+
+[1, 2, 3].join          #=> "123"
+[1, 2, 3].join("-")     #=> "1-2-3"
+
+
+
+
