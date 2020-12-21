7e071302d66f9d986f8098fca24206e721a4e3ca
realisticattorney <g.axl.aquila@gmail.com>
2020-12-20 22:09:04 -0300

    add types of variables

--- rubyRepaso.rb
+++ rubyRepaso.rb
@@ -198,3 +198,35 @@ end
 
 puts a       # is it accessible here? 
 The answer is yes. The reason is because the for...do/end code did not create a new inner scope, since for is part of Ruby language and not a method invocation. When we use each, times and other method invocations, followed by {} or do/end, thats when a new block is created.
+
+
+
+  Types of variables
+
+  Constants are declared by capitalizing every letter in the variable's name, per Ruby convention. They are used for storing data that never needs to change. While most programming languages do not allow you to change the value assigned to a constant, Ruby does.It will however throw a warning letting you know that there was a previous definition for that variable.
+
+     MY_CONSTANT = 'I am available throughout your app.'
+
+     Global variables are declared by starting the variable name with the dollar sign ($). These variables are available throughout your entire app, overriding all scope boundaries. Rubyists tend to stay away from global variables as there can be unexpected complications when using them.
+     $var = 'I am also available throughout your app.'
+
+     Class variables are declared by starting the variable name with two @ signs. These variables are accessible by instances of your class, as well as the class itself. When you need to declare a variable that is related to a class, but each instance of that class does not need its own value for this variable, you use a class variable
+'
+     @@instances = 0
+
+     Instance variables are declared by starting the variable name with one @ sign. These variables are available throughout the current instance of the parent class. Instance variables can cross some scope boundaries, but not all of them.
+
+     @var = 'I am available throughout the current instance of this class.'
+
+
+Local variables are the most common variables you will come across and obey all scope boundaries. These variables are declared by starting the variable name with neither $ nor @, as well as not capitalizing the entire variable name.
+
+Example of a local variable declaration:
+
+Copy Code
+var = 'I must be passed around to cross scope boundaries.'
+
+
+
+
+
