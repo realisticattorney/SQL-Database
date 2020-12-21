e886c52ad0850e6625be1d06b3fb5367bdc5f471
realisticattorney <g.axl.aquila@gmail.com>
2020-12-20 22:14:06 -0300

    add  upto downto  loop

--- rubyRepaso.rb
+++ rubyRepaso.rb
@@ -435,3 +435,17 @@ end
 
 If you need to run a loop for a specified number of times, then look no further than the trusty #times loop. 
 
+
+
+
+
+Upto and Downto Loops
+
+5.upto(10) {|num| print "#{num} " }     #=> 5 6 7 8 9 10
+
+10.downto(5) {|num| print "#{num} " }   #=> 10 9 8 7 6 5
+
+You can use these methods to iterate from a starting number either up to or down to another number, respectively.
+
+
+
