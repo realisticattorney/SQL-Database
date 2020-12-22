b562753d760ab16e8e9d8b159ce05f93c410f664
realisticattorney <g.axl.aquila@gmail.com>
2020-12-22 19:18:48 -0300

    add sum and count up to ex n6

--- .history/aftercaptsonepractice_20201222191835.rb
+++ .history/aftercaptsonepractice_20201222191835.rb
@@ -372,3 +372,17 @@ SELECT continent, count(name)
   FROM world
  GROUP BY continent
 
+
+7
+ SELECT continent, count(name)
+  FROM world
+where population >= 10000000
+ GROUP BY continent
+
+
+8
+ SELECT continent
+  FROM world
+ GROUP BY continent
+having sum(population) >= 100000000
+
